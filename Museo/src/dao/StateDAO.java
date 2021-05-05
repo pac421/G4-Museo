98029d3f19a944cb3a71180de488371f38375834
Paul-Alexandre CIESLIK <paul-alexandre.cieslik@hotmail.com>
2021-05-05 14:02:03 +0200

    Added find propertyState functions

--- Museo/src/dao/StateDAO.java
+++ Museo/src/dao/StateDAO.java
@@ -1,11 +1,10 @@
 package dao;
 
+import bean.Property;
+import bean.PropertyState;
 import bean.State;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class StateDAO extends DAO<State> {
@@ -110,5 +109,37 @@ public class StateDAO extends DAO<State> {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public ArrayList<PropertyState> findAllProperty(State state) {
+        try {
+            String sql = "SELECT * FROM PROPERTY_STATE PS INNER JOIN PROPERTY P ON PS.property_id = P.id WHERE PS.state_id = ?";
+            PreparedStatement statement = connect.prepareStatement(sql);
+            statement.setString(1, state.getId());
+            ResultSet result = statement.executeQuery();
+
+            ArrayList<PropertyState> propertyStates = new ArrayList<PropertyState>();
+
+            while (result.next()) {
+                Date start = result.getDate("start");
+                Date end = result.getDate("end");
+                String comment = result.getString("comment");
+                String property_id = result.getString("property_id");
+
+                DAO<Property> propertyDAO = new DAOFactory().getPropertyDAO();
+                Property property_obj = propertyDAO.find(property_id);
+
+                PropertyState propertyState = new PropertyState(property_obj, state, start, end, comment);
+                propertyStates.add(propertyState);
+            }
+
+            result.close();
+            statement.close();
+
+            return propertyStates;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
