775c4ec10f844227d516baba6ac77e204b580420
Paul-Alexandre CIESLIK <paul-alexandre.cieslik@hotmail.com>
2021-06-17 20:17:01 +0200

    Added property and lend form mechanism

--- Museo/src/WorkForm.java
+++ Museo/src/WorkForm.java
@@ -1,6 +1,4 @@
-import bean.Category;
-import bean.Collection;
-import bean.Work;
+import bean.*;
 import dao.DAO;
 import dao.DAOFactory;
 import javax.swing.*;
@@ -11,6 +9,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class WorkForm extends JPanel {
+    JLabel ownedAtLabel, ownedFromLabel, priceLabel, startLabel, endLabel, lenderLabel;
+    JTextField ownedAtField, ownedFromField, priceField, startField, endField, lenderField;
 
     public WorkForm(JTable table, JButton add_btn, JButton edit_btn, JButton del_btn, JButton clear_btn) {
         this.setBorder(new EmptyBorder(20, 0, 10, 0));
@@ -18,6 +18,25 @@ public class WorkForm extends JPanel {
 
         Font labelFont = new Font("Montserrat", Font.PLAIN, 16);
         Font fieldFont = new Font("Montserrat", Font.PLAIN, 14);
+
+        JLabel typeLabel = new JLabel("Type");
+        typeLabel.setFont(labelFont);
+        JPanel typeField = new JPanel();
+        typeField.setBackground(Color.white);
+        ButtonGroup bg = new ButtonGroup();
+
+        JRadioButton rbProperty = new JRadioButton("Propriété");
+        rbProperty.setBackground(Color.white);
+        rbProperty.setSelected(true);
+        rbProperty.setFont(fieldFont);
+        bg.add(rbProperty);
+        typeField.add(rbProperty);
+
+        JRadioButton rbLend = new JRadioButton("Prêt");
+        rbLend.setBackground(Color.white);
+        rbLend.setFont(fieldFont);
+        bg.add(rbLend);
+        typeField.add(rbLend);
 
         JLabel titleLabel = new JLabel("Titre");
         titleLabel.setFont(labelFont);
@@ -80,6 +99,42 @@ public class WorkForm extends JPanel {
         }
         collectionList.getModel().setSelectedItem("");
 
+        ownedAtLabel = new JLabel("Date d'acquisition");
+        ownedAtLabel.setFont(labelFont);
+        ownedAtField = new JTextField(20);
+        ownedAtField.setFont(fieldFont);
+
+        ownedFromLabel = new JLabel("Nom du cessionnaire");
+        ownedFromLabel.setFont(labelFont);
+        ownedFromField = new JTextField(20);
+        ownedFromField.setFont(fieldFont);
+
+        priceLabel = new JLabel("Prix (€)");
+        priceLabel.setFont(labelFont);
+        priceField = new JTextField(20);
+        priceField.setFont(fieldFont);
+
+        startLabel = new JLabel("Date d'acquisition");
+        startLabel.setFont(labelFont);
+        startLabel.setVisible(false);
+        startField = new JTextField(20);
+        startField.setFont(fieldFont);
+        startField.setVisible(false);
+
+        endLabel = new JLabel("Date de rente");
+        endLabel.setFont(labelFont);
+        endLabel.setVisible(false);
+        endField = new JTextField(20);
+        endField.setFont(fieldFont);
+        endField.setVisible(false);
+
+        lenderLabel = new JLabel("Nom du cessionnaire");
+        lenderLabel.setFont(labelFont);
+        lenderLabel.setVisible(false);
+        lenderField = new JTextField(20);
+        lenderField.setFont(fieldFont);
+        lenderField.setVisible(false);
+
         GroupLayout groupLayout = new GroupLayout(this);
         this.setLayout(groupLayout);
 
@@ -88,6 +143,7 @@ public class WorkForm extends JPanel {
 
         GroupLayout.SequentialGroup horizontalGroup = groupLayout.createSequentialGroup();
         horizontalGroup.addGroup(groupLayout.createParallelGroup()
+                .addComponent(typeLabel)
                 .addComponent(titleLabel)
                 .addComponent(descriptionLabel)
                 .addComponent(periodLabel)
@@ -97,8 +153,15 @@ public class WorkForm extends JPanel {
                 .addComponent(weightLabel)
                 .addComponent(categoryLabel)
                 .addComponent(collectionLabel)
+                .addComponent(ownedAtLabel)
+                .addComponent(ownedFromLabel)
+                .addComponent(priceLabel)
+                .addComponent(startLabel)
+                .addComponent(endLabel)
+                .addComponent(lenderLabel)
         );
         horizontalGroup.addGroup(groupLayout.createParallelGroup()
+                .addComponent(typeField)
                 .addComponent(titleField)
                 .addComponent(descriptionField)
                 .addComponent(periodField)
@@ -108,10 +171,17 @@ public class WorkForm extends JPanel {
                 .addComponent(weightField)
                 .addComponent(categoryList)
                 .addComponent(collectionList)
+                .addComponent(ownedAtField)
+                .addComponent(ownedFromField)
+                .addComponent(priceField)
+                .addComponent(startField)
+                .addComponent(endField)
+                .addComponent(lenderField)
         );
         groupLayout.setHorizontalGroup(horizontalGroup);
 
         GroupLayout.SequentialGroup verticalGroup = groupLayout.createSequentialGroup();
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(typeLabel).addComponent(typeField));
         verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(titleLabel).addComponent(titleField));
         verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(descriptionLabel).addComponent(descriptionField));
         verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(periodLabel).addComponent(periodField));
@@ -121,9 +191,17 @@ public class WorkForm extends JPanel {
         verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(weightLabel).addComponent(weightField));
         verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(categoryLabel).addComponent(categoryList));
         verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(collectionLabel).addComponent(collectionList));
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(ownedAtLabel).addComponent(ownedAtField));
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(ownedFromLabel).addComponent(ownedFromField));
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(priceLabel).addComponent(priceField));
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(startLabel).addComponent(startField));
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(endLabel).addComponent(endField));
+        verticalGroup.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(lenderLabel).addComponent(lenderField));
         groupLayout.setVerticalGroup(verticalGroup);
 
         DAO<Work> workDAO = new DAOFactory().getWorkDAO();
+        DAO<Property> propertyDAO = new DAOFactory().getPropertyDAO();
+        DAO<Lend> lendDAO = new DAOFactory().getLendDAO();
 
         table.getSelectionModel().addListSelectionListener(event -> {
             if (!event.getValueIsAdjusting() && table.getSelectedRow() != -1){
@@ -146,6 +224,27 @@ public class WorkForm extends JPanel {
                 weightField.setText(String.valueOf(work.getWeight()));
                 categoryList.getModel().setSelectedItem(work.getCategory().getLabel());
                 collectionList.getModel().setSelectedItem(work.getCollection().getLabel());
+
+                Property property = propertyDAO.find(id);
+                Lend lend = lendDAO.find(id);
+
+                if (property != null) {
+                    rbProperty.setSelected(true);
+                    rbLend.setSelected(false);
+                    showPropertyForm(true);
+
+                    ownedAtField.setText(String.valueOf(property.getOwnedAt()));
+                    ownedFromField.setText(property.getOwnedFrom());
+                    priceField.setText(String.valueOf(property.getPrice()));
+                } else {
+                    rbProperty.setSelected(false);
+                    rbLend.setSelected(true);
+                    showPropertyForm(false);
+
+                    startField.setText(String.valueOf(lend.getStart()));
+                    endField.setText(String.valueOf(lend.getEnd()));
+                    lenderField.setText(lend.getLender());
+                }
             }
         });
 
@@ -168,7 +267,40 @@ public class WorkForm extends JPanel {
             weightField.setText("");
             categoryList.getModel().setSelectedItem("");
             collectionList.getModel().setSelectedItem("");
+
+            ownedAtField.setText("");
+            ownedFromField.setText("");
+            priceField.setText("");
+            startField.setText("");
+            endField.setText("");
+            lenderField.setText("");
         });
+
+        rbProperty.addActionListener(e -> {
+            System.out.println("property selected");
+            showPropertyForm(true);
+        });
+
+        rbLend.addActionListener(e -> {
+            System.out.println("lend selected");
+            showPropertyForm(false);
+        });
+    }
+
+    private void showPropertyForm(Boolean state) {
+        ownedAtLabel.setVisible(state);
+        ownedAtField.setVisible(state);
+        ownedFromLabel.setVisible(state);
+        ownedFromField.setVisible(state);
+        priceLabel.setVisible(state);
+        priceField.setVisible(state);
+
+        startLabel.setVisible(!state);
+        startField.setVisible(!state);
+        endLabel.setVisible(!state);
+        endField.setVisible(!state);
+        lenderLabel.setVisible(!state);
+        lenderField.setVisible(!state);
     }
 
     static class ItemRenderer extends BasicComboBoxRenderer {
